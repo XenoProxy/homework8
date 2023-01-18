757438892d1cafb6e4eb487e9525df9f7bf278ee
XenoProxy <44860152+XenoProxy@users.noreply.github.com>
2023-01-18 14:04:44 +0300

    [+] getProducts and showProducts funcs

--- elements/functions.php
+++ elements/functions.php
@@ -1,21 +1,43 @@
 <?php
 
-$ch = curl_init();
+function getProducts(){
+    $ch = curl_init();
+    $options = [
+        CURLOPT_URL => "https://fakestoreapi.com/products",
+        CURLOPT_RETURNTRANSFER => true
+    ];
 
-curl_setopt($ch, CURLOPT_URL, "https://fakerapi.it/api/v1/products?");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt_array($ch, $options);
 
-$result = curl_exec($ch);
-
-if(curl_errno($ch))
-{
-    echo 'Ошибка curl: ' . curl_error($ch);
+    $result = curl_exec($ch);
+    $errors = curl_errno($ch);
+    if($errors)
+    {
+        echo 'Errors  (' . $errors . '): ' . curl_error($ch);
+    } else {
+        return json_decode($result, true);
+    }
+    curl_close($ch);    
 }
 
-curl_close($ch);
+function showProducts(){
+    $data = getProducts();
 
-$data = json_decode($result, true);
+    $html = '';
+    foreach($data as $product){
+        $html .= '
+            <div class="product">
+                <img src="' . $product['image'] . '" alt="' . $product['title'] . '"/>
+                <h3>' . $product['title'] . '</h3>
+                <div class="product_price">' . $product['price'] . '</div>
+            </div>
+        ';
+    }
 
-echo "<pre>";
-print_r($data);
-echo "</pre>";
+    if (!empty($html)) {
+        echo '<div class="product_list">'. $html .'</div>';
+    }
+    // echo '<pre>';
+    // print_r($data);
+    // echo '</pre>';       
+}
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?php require_once 'elements/header.php'; ?>
+<?php require_once 'elements/header.php'; ?> 
+<?php require_once 'elements/functions.php'; ?>
 
-
+<h1 class="title">Catalog</h1>
+<section class="main">
+    <?php showProducts();?>
+</section>
 
 <?php require_once 'elements/footer.php';
