f685a3e51bcc93134c6c43ea9d270ffadc872c31
XenoProxy <44860152+XenoProxy@users.noreply.github.com>
2023-01-18 14:09:54 +0300

    [+] style.css

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php require_once 'elements/header.php'; ?> 
 <?php require_once 'elements/functions.php'; ?>
 
+<link rel="stylesheet" href="css/style.css">
 <h1 class="title">Catalog</h1>
 <section class="main">
     <?php showProducts();?>
