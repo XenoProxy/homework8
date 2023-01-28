bec0f72daefb8292a4fe9809ea7f8f24c1895dcc
XenoProxy <44860152+XenoProxy@users.noreply.github.com>
2023-01-28 15:47:40 +0300

    [+] site logo

--- elements/header.php
+++ elements/header.php
@@ -6,12 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="../js/scripts.js"></script>
-    <title>HW8</title>    
+    <title>MockyMarket</title>    
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
     <?php require_once 'functions.php'; ?>
     <header class="header">
-        <h1>HW8</h1>
+        <div class="logo">
+            <img src="../img/logo.png" alt="Logo">
+            <h1>MockyMarket</h1>
+        </div>        
         <?php addProdToCart() ?>
     </header>
