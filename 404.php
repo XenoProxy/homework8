59b0bd9cc631a718f749ac547a85ad22f2eebc29
XenoProxy <44860152+XenoProxy@users.noreply.github.com>
2023-01-28 13:45:37 +0300

    [+] 404 error page

--- 404.php
+++ 404.php
@@ -1,5 +1,9 @@
 <?php require_once 'elements/header.php'; ?>
 
-<h1>404</h1>
+<style>footer{margin-top: 0;}</style>
+<link rel="stylesheet" href="css/style.css">
+<div class="banner">
+    <h1 class="title">Page not found</h1>
+</div> 
 
 <?php require_once 'elements/footer.php';
