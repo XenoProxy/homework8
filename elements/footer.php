856c6f3934f59ab9014e47a51a0f0464ea742601
XenoProxy <44860152+XenoProxy@users.noreply.github.com>
2023-01-28 13:45:45 +0300

    [+] footer

---
+++ elements/footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="footer">
+        <div class="contact">
+            <h4>CONTACT US</h4>
+            <div>
+                <!-- <img src="../img/location.png" alt=""> -->
+                <p>55 Main St.<br>Toronto</p>
+            </div>
+            <div>
+                <!-- <img src="../img/phone.png" alt=""> -->
+                <p>+375291234567</p>
+            </div>
+            <div>
+                <!-- <img src="../img/mail.png" alt=""> -->
+                <p>info@gmail.com</p>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>&copy; Copyright 2022</p>
+        </div> 
+    </div>
+</footer>
